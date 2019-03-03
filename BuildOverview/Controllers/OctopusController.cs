c9982ddfc2094f472f92c77fbe203fa133afca1a
Administrator <gurdipsira_1@yahoo.co.uk>
2019-03-02 17:28:43 -0800

    NA

---
+++ BuildOverview/Controllers/OctopusController.cs
@@ -0,0 +1,68 @@
+﻿using Octopus.Client;
+using Octopus.Client.Model;
+using Octopus.Client.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BuildOverview.Controllers
+{
+    public class OctopusController : Controller
+    {
+        // GET: Octopus
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public List<ProjectResource> GetProjects()
+        {
+            var server = "https://gurdip-sira.octopus.app/";
+            var apiKey = "API-FVRZ6YTGFVWV5YJHMJL9A58BKXK";             // Get this from your 'profile' page in the Octopus web portal
+            var endpoint = new OctopusServerEndpoint(server, apiKey);
+            using (var client = OctopusAsyncClient.Create(endpoint))
+            {
+                var repository = new OctopusRepository(endpoint);
+                List<ProjectResource> prs = repository.Projects.GetAll();
+
+                return prs;
+
+            }
+        }
+
+        public List<EnvironmentResource> GetEnvironments()
+        {
+            var server = "https://gurdip-sira.octopus.app/";
+            var apiKey = "API-FVRZ6YTGFVWV5YJHMJL9A58BKXK";             // Get this from your 'profile' page in the Octopus web portal
+            var endpoint = new OctopusServerEndpoint(server, apiKey);
+            using (var client = OctopusAsyncClient.Create(endpoint))
+            {
+                var repository = new OctopusRepository(endpoint);
+                List<EnvironmentResource> envs = repository.Environments.GetAll();
+
+              
+
+                return envs;
+
+            }
+        }
+
+        public void GetEnvironmentData(List<EnvironmentResource> er)
+        {
+            er.
+        }
+
+        public void GetProjectData(List<ProjectResource> projectResources)
+        {
+           foreach (ProjectResource project in projectResources)
+            {
+                project.Name;
+                project.ProjectConnectivityPolicy.TargetRoles;
+               
+            }
+        }
+    }
+}
---
+++ BuildOverview/CreateVm.ps1
@@ -0,0 +1,50 @@
+﻿
+
+$vm = New-AzVm `
+    -ResourceGroupName "myResourceGroup" `
+    -Name "myVM" `
+    -Location "East US" `
+    -VirtualNetworkName "myVnet" `
+    -SubnetName "mySubnet" `
+    -SecurityGroupName "myNetworkSecurityGroup" `
+    -PublicIpAddressName "myPublicIpAddress" `
+    -OpenPorts 80,3389
+
+
+$publicSettings = @{
+  OctopusServerUrl = "https://octopus.example.com";
+  Environments = @("Env1", "Env2");
+  Roles = @("app-server", "web-server");
+  CommunicationMode = "Listen";
+  Port = 10933
+}
+
+$privateSettings = @{"ApiKey" = "<MY SECRET API KEY>"}
+
+Set-AzureRmVMExtension -ResourceGroupName "<resource-group-name>" `
+    -Location "Australia East" `
+    -VMName "<vm-name>" `
+    -Name "OctopusDeployWindowsTentacle" `
+    -Publisher "OctopusDeploy.Tentacle" `
+    -TypeHandlerVersion "2.0" `
+    -Settings $publicSettings `
+    -ProtectedSettings $privateSettings `
+    -ExtensionType "OctopusDeployWindowsTentacle"
+
+# optional - add an NSG rule to allow the Octopus Server to contact the Tentacle
+# only required in Listening mode
+$vm = Get-AzureRmVm -Name "<vm-name>" -ResourceGroupName "<resource-group-name>"
+$nic = Get-AzureRmNetworkInterface -ResourceGroupName "<resource-group-name>" | ? { $_.VirtualMachine.Id -eq $vm.Id -and $_.Primary }
+$secGrp = Get-AzureRmNetworkSecurityGroup -ResourceGroupName "<resource-group-name>" | ? { $_.Id -eq $nic.NetworkSecurityGroup.Id }
+$secGrp | Add-AzureRmNetworkSecurityRuleConfig `
+    -Name "AllowTentacleInBound" `
+    -Description "Allow inbound traffic to Tentacle" `
+    -Protocol TCP `
+    -SourcePortRange "*" `
+    -SourceAddressPrefix "*" `
+    -DestinationPortRange 10933 `
+    -DestinationAddressPrefix "*" `
+    -Access Allow `
+    -Priority 999 `
+    -Direction Inbound
+$secGrp | Set-AzureRmNetworkSecurityGroup
